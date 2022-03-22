8697dfd7f6b4cfd517cd5bb19f01baba1ae9b65e
bronickone <bronickone@gmail.com>
2022-03-22 17:00:43 +0200

    Add buttons and styles to app

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const container = document.querySelector('.container');
-let size = 16;
+let size;
+let sizePrev;
 
 function drawSquare(){
+    size = document.getElementById("size").value;
+    sizePrev = size;                         // Value for field cleaner
     for (let j=1; j <= size; j++){
         const column = document.createElement('div');
         column.classList.add('column');
@@ -19,21 +22,62 @@ function drawSquare(){
 
 drawSquare();
 
-const clearButton = document.querySelector('.clear');
+const clearButton = document.querySelector('.clearButton');
+const acceptButton = document.querySelector('.acceptButton');
+const changeButton = document.querySelector('.changeButton');
+const coloredRow = document.querySelectorAll('.coloredRow');
+
+clearButton.addEventListener('click', () => {      
+    clearField();
+    drawSquare();    
+});   
+
+acceptButton.addEventListener('click', () => {   
+    size = document.getElementById("size").value;
+    enterSize();  
+});   
+
+changeButton.addEventListener('click', () => {   
+    color = document.getElementById("color").value;
+    coloredRow.style.backgroundColor = color;
+    ;     
+});   
 
 function clearField(){
-    for (let j=1; j <= size; j++){
+    for (let j=1; j <= sizePrev; j++){
         const column = document.querySelector('.column')
         container.removeChild(column);
     }
 }
 
-clearButton.addEventListener('click', () => {   
-    clearField();
-    size = prompt('Enter Size');
-    drawSquare();  
-});   
+const notValid = document.querySelector('.notValid');
+
+function enterSize(){
+    if (size > 0 && size < 101){
+        clearField();
+        drawSquare();
+        notValid.textContent = ''; 
+    }
+    else {   
+        notValid.textContent = 'Not valid value';
+    } 
+} 
 
 
+
+
+
+
+
+
+function changeColor(){
+
+}
+
+const randColor = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+}
+  
 
 
